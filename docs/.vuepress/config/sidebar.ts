6e6de40270c56ee3298be55c0eb446cbd491eed6
SalHe Li <SalHe@qq.com>
2022-04-04 10:56:25 +0800

    docs: 给测试文档添加导航

--- docs/.vuepress/config/sidebar.ts
+++ docs/.vuepress/config/sidebar.ts
@@ -1,28 +1,39 @@
 import type { SidebarConfig } from '@vuepress/theme-default'
 
-export const sidebar: SidebarConfig = [
-  {
-    text: '团队',
-    collapsible: true,
-    children: [
-      '/team',
-    ]
-  },
-  {
-    text: '项目',
-    collapsible: true,
-    children: [
-      '/project/overview',
-      '/project/plan',
-      '/project/requirement-analysis',
-    ]
-  },
-  {
-    text: '会议记录',
-    collapsible: true,
-    children: [
-      '/meeting/2022-03-26',
-      '/meeting/2022-04-02',
-    ]
-  }
-];
+export const sidebar: SidebarConfig = {
+  '/': [
+    {
+      text: '团队',
+      collapsible: true,
+      children: [
+        '/team',
+      ]
+    },
+    {
+      text: '项目',
+      collapsible: true,
+      children: [
+        '/project/overview',
+        '/project/plan',
+        '/project/requirement-analysis',
+      ]
+    },
+    {
+      text: '会议记录',
+      collapsible: true,
+      children: [
+        '/meeting/2022-03-26',
+        '/meeting/2022-04-02',
+      ]
+    }
+  ],
+  '/test': [
+    {
+      text: 'Markdown测试',
+      collapsible: true,
+      children: [
+        '/test/mermaid',
+      ]
+    }
+  ]
+};
