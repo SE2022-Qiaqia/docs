bcfe4661cef2e8620c23ed42a0b1149b9b331bb1
SalHe Li <SalHe@qq.com>
2022-04-04 10:43:49 +0800

    fix: 调整网站icon地址

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,12 +3,15 @@ import { mermaidPlugin } from "./plugins/mermaid";
 import { navbar } from "./config/navbar";
 import { sidebar } from "./config/sidebar";
 
+const base = '/docs/';
+
 export default defineUserConfig<DefaultThemeOptions>({
-  base: '/docs/',
+  base,
   title: '2022SE Qiaqia',
   description: '2022软工小组',
   head: [
-    ['link', { rel: 'icon', href: '/images/hero.jpg' }],
+    // 这里需要手动加上base，暂时没找到有没有比较好的做法
+    ['link', { rel: 'icon', href: `${base}images/hero.jpg` }],
   ],
   plugins: [
     [
