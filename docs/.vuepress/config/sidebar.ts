5e52295b3527dd1eccf6e5e40d6bf90c92fdd50e
SalHe Li <SalHe@qq.com>
2022-04-04 10:53:17 +0800

    chore: change spaces to 2

--- docs/.vuepress/config/sidebar.ts
+++ docs/.vuepress/config/sidebar.ts
@@ -1,28 +1,28 @@
 import type { SidebarConfig } from '@vuepress/theme-default'
 
 export const sidebar: SidebarConfig = [
-    {
-        text: '团队',
-        collapsible: true,
-        children: [
-            '/team',
-        ]
-    },
-    {
-        text: '项目',
-        collapsible: true,
-        children: [
-            '/project/overview',
-            '/project/plan',
-            '/project/requirement-analysis',
-        ]
-    },
-    {
-        text: '会议记录',
-        collapsible: true,
-        children: [
-            '/meeting/2022-03-26',
-            '/meeting/2022-04-02',
-        ]
-    }
+  {
+    text: '团队',
+    collapsible: true,
+    children: [
+      '/team',
+    ]
+  },
+  {
+    text: '项目',
+    collapsible: true,
+    children: [
+      '/project/overview',
+      '/project/plan',
+      '/project/requirement-analysis',
+    ]
+  },
+  {
+    text: '会议记录',
+    collapsible: true,
+    children: [
+      '/meeting/2022-03-26',
+      '/meeting/2022-04-02',
+    ]
+  }
 ];
