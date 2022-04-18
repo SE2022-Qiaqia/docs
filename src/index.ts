6291a1253ad00b88534c79fdbf4336a60648026b
SalHe Li <SalHe@qq.com>
2022-04-18 18:34:07 +0800

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { createDevApp } from 'vuepress';
+import { createDevApp, resolveDevUserConfig, resolveUserConfigConventionalPath, transformUserConfigToPlugin, allowTs } from 'vuepress';
 import puppeteer from 'puppeteer';
+import { path } from "@vuepress/utils";
+import fs from 'fs';
+
+async function createDevAppFromSource(sourcePath: string) {
+  allowTs();
+
+  const appConfig = { source: path.resolve(sourcePath) };
+  const userConfigPath = resolveUserConfigConventionalPath(appConfig.source);
+  const { userConfig } = await resolveDevUserConfig(userConfigPath);
+
+  const app = createDevApp({
+    ...appConfig,
+    ...userConfig,
+  });
+
+  app.use(transformUserConfigToPlugin(app, userConfig));
+
+  return app;
+}
 
 const start = async () => {
-  const app = createDevApp({
-    host: 'localhost',
-    port: 8080,
-    source: './docs',
-    base: '/docs/'
-  });
+
+  const app = await createDevAppFromSource("./docs");
 
   await app.init();
   await app.prepare();
@@ -16,17 +31,25 @@ const start = async () => {
 
   const browser = await puppeteer.launch();
   const browserPage = await browser.newPage();
-  const { host, port, base } = app.options;
+  const { port, base } = app.options;
+  const host = app.options.host === '0.0.0.0' ? 'localhost' : app.options.host;
 
   // TODO：确定导出PDF的顺序
+  const sidebarItems = resolveSidebarItems(app.options.themeConfig.sidebar);
+  console.log(sidebarItems);
+
+
   // TODO：添加PDF目录
   // TODO：添加PDF页码、页眉等
   // TODO：合并PDF
 
+  const pdfOutputDir = app.dir.temp("pdf");
+  if (!fs.existsSync(pdfOutputDir)) fs.mkdirSync(pdfOutputDir);
+
   for (const page of app.pages.filter(page => page.path !== '/404.html')) {
     const url = `http://${host}:${port}${base}${page.path.slice(1)}`;
     await browserPage.goto(url, { waitUntil: 'networkidle2' });
-    await browserPage.pdf({ path: app.dir.temp(`${page.key}.pdf`), format: 'a4' });
+    await browserPage.pdf({ path: app.dir.temp('pdf', `${page.key}.pdf`), format: 'a4' });
     console.log(`Generated ${page.key}.pdf, 《${page.title}》, ${url}`);
   }
 
